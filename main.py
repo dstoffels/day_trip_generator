9705967714f9fbc58dd1917682adc2ce1304a56f
dstoffels <dan.stoffels@gmail.com>
2023-03-28 10:07:57 -0500

    feat: implemented generate_day_trip()

--- main.py
+++ main.py
@@ -1,28 +1,29 @@
 import random
 
-destinations = ['Maui', 'Hong Kong', 'Sydney', 'Capetown', 'London', 'Fiji']
-restaurants = ['The Leaky Cauldron', 'The Inn of the Prancing Pony', 'Mos Isley Cantina', 'Ten Forward Lounge', 'Krabby Patty', 'Arbys']
-transportations = ['rickshaw', 'horseback', 'hoverboard', 'jetpack', 'piggyback', 'flying carpet']
-entertainments = ['bar hopping', 'skydiving', 'parasailing', 'snorkeling', 'spelunking', 'backpacking']
+def generate_day_trip():
+    destinations = ['Maui', 'Hong Kong', 'Sydney', 'Capetown', 'London', 'Fiji']
+    restaurants = ['The Leaky Cauldron', 'The Inn of the Prancing Pony', 'Mos Isley Cantina', 'Ten Forward Lounge', 'Krabby Patty', 'Arbys']
+    transportations = ['rickshaw', 'horseback', 'hoverboard', 'jetpack', 'piggyback', 'flying carpet']
+    entertainments = ['bar hopping', 'skydiving', 'parasailing', 'snorkeling', 'spelunking', 'backpacking']
 
-
-# (15 points): As a user, I want to be able to randomly re-select a destination, restaurant, mode of transportation, 
-# and/or form of entertainment if I don’t like one or more of those things.
-
-user_response = ''
-
-while user_response != 'y':
-    # generate/regenerate the day trip
     destination = random.choice(destinations)
     restaurant = random.choice(restaurants)
     transportation = random.choice(transportations)
     entertainment = random.choice(entertainments)
+
+    return [destination, restaurant, transportation, entertainment]
+
+
+user_response = ''
+
+while user_response != 'y':
+    day_trip_list = generate_day_trip()
     
     # display the day trip to the user in the terminal (print each variable)
     print()
     print("Here's your day trip:")
-    print(f"You'll be going to {destination} by {transportation}.")
-    print(f"You'll be dining at {restaurant} after {entertainment}.")
+    print(f"You'll be going to {day_trip_list[0]} by {day_trip_list[2]}.")
+    print(f"You'll be dining at {day_trip_list[1]} after {day_trip_list[3]}.")
     print()
 
     # ask user if they're satisfied with the day trip (input function)
