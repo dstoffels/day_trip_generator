57f61f79461a40e7ad09bdbaab8afde613ca0adc
dstoffels <dan.stoffels@gmail.com>
2023-03-28 10:20:01 -0500

    refactor: organized all code into functions

--- main.py
+++ main.py
@@ -13,22 +13,23 @@ def generate_day_trip():
 
     return [destination, restaurant, transportation, entertainment]
 
-
-user_response = ''
-
-while user_response != 'y':
-    day_trip_list = generate_day_trip()
-    
-    # display the day trip to the user in the terminal (print each variable)
+def display_day_trip(day_trip_options):
     print()
     print("Here's your day trip:")
-    print(f"You'll be going to {day_trip_list[0]} by {day_trip_list[2]}.")
-    print(f"You'll be dining at {day_trip_list[1]} after {day_trip_list[3]}.")
+    print(f"You'll be going to {day_trip_options[0]} by {day_trip_options[2]}.")
+    print(f"You'll be dining at {day_trip_options[1]} after {day_trip_options[3]}.")
     print()
+    
 
-    # ask user if they're satisfied with the day trip (input function)
-    user_response = input('Are you satisfied with your day trip? y/n ')
+def confirm_trip():
+    user_response = ''
+
+    while user_response != 'y':
+        day_trip_list = generate_day_trip()   
+        display_day_trip(day_trip_list)
+        user_response = input('Are you satisfied with your day trip? y/n ')
+    print()
+    print('Enjoy your trip! Goodbye...')
 
 
-# --- if user's response is yes, complete trip (aka break the loop)
-# --- if user's response is no, go back to step 1 (while loop, condition ???)
+confirm_trip()
