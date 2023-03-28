a3cfff807cc759dbce9adfba0618875394277e63
dstoffels <dan.stoffels@gmail.com>
2023-03-28 09:34:33 -0500

    feat: added lists for trip options; implemented random selections from each

---
+++ main.py
@@ -0,0 +1,11 @@
+import random
+
+destinations = ['Maui', 'Hong Kong', 'Sydney', 'Capetown', 'London', 'Fiji']
+restaurants = ['The Leaky Cauldron', 'The Inn of the Prancing Pony', 'Mos Isley Cantina', 'Ten Forward Lounge', 'Krabby Patty', 'Arbys']
+transportations = ['rickshaw', 'horseback', 'hoverboard', 'jetpack', 'piggyback', 'flying carpet']
+entertainments = ['bar hopping', 'skydiving', 'parasailing', 'snorkeling', 'spelunking', 'backpacking']
+
+destination = random.choice(destinations)
+restaurant = random.choice(restaurants)
+transportation = random.choice(transportations)
+entertainment = random.choice(entertainments)
